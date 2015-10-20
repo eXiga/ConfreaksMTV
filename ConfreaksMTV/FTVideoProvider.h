87faad5be88155983a6d8b30be180ce623007631
eXiga <anton.kostenich@hotmail.com>
2015-10-21 00:02:29 +0300

    Implement video provider

--- ConfreaksMTV/FTVideoProvider.h
+++ ConfreaksMTV/FTVideoProvider.h
@@ -9,6 +9,15 @@
 #import <Foundation/Foundation.h>
 #import "FTBaseProvider.h"
 
+FOUNDATION_EXPORT NSString *const VideosEndpointName;
+FOUNDATION_EXPORT NSString *const VideosCountResourceName;
+FOUNDATION_EXPORT NSString *const VideoFeaturedResourceName;
+
 @interface FTVideoProvider : FTBaseProvider
+
+- (void)getAllEntitiesUsingLimit:(NSNumber *)limit orderedByDescending:(BOOL)ordered withCompletionHandler:(FTResponseHandler)handler;
+- (void)getVideoCountWithCompletionHandler:(FTResponseHandler)handler;
+- (void)getFeaturedVideoWithCompletionHandler:(FTResponseHandler)handler;
+- (void)getVideosForPresenter:(id)presenterId atEvent:(id)eventId withCompletionHandler:(FTResponseHandler)handler;
 
 @end
--- ConfreaksMTV/FTVideoProvider.m
+++ ConfreaksMTV/FTVideoProvider.m
@@ -7,7 +7,71 @@
 //
 
 #import "FTVideoProvider.h"
+#import "FTResourceDownloader.h"
+#import "FTPresenterProvider.h"
+#import "FTEventProvider.h"
+
+NSString *const VideosEndpointName = @"videos";
+NSString *const VideosCountResourceName = @"video_count";
+NSString *const VideoFeaturedResourceName = @"featured-video";
 
 @implementation FTVideoProvider
+
+- (instancetype)init {
+    self = [super initWithEndpointName:VideosEndpointName];
+    
+    if (self) {
+        
+    }
+    
+    return self;
+}
+
+// You probably should NEVER use this method, cuz JSON is big af
+- (void)getAllEntitiesWithCompletionHandler:(FTResponseHandler)handler {
+    NSURLRequest *request = [NSURLRequest requestWithURL:self.url];
+    [FTResourceDownloader getRequest:request withHandler:handler];
+}
+
+- (void)getAllEntitiesUsingLimit:(NSNumber *)limit orderedByDescending:(BOOL)ordered withCompletionHandler:(FTResponseHandler)handler {
+    NSString *queryString = [NSString stringWithFormat:@"limit=%i", [limit integerValue]];
+    
+    if (ordered) {
+        queryString = [NSString stringWithFormat:@"%@&sort=most_viewed", queryString];
+    }
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@?%@", [self.url absoluteString], queryString]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [FTResourceDownloader getRequest:request withHandler:handler];
+}
+
+- (void)getEntityForId:(id)entityId withCompletionHandler:(FTResponseHandler)handler {
+    NSString *videoId = [entityId isKindOfClass:[NSString class]] ? (NSString *)entityId : [entityId stringValue];
+    NSURL *videoUrl = [self.url URLByAppendingPathComponent:videoId];
+    NSURLRequest *request = [NSURLRequest requestWithURL:videoUrl];
+    [FTResourceDownloader getRequest:request withHandler:handler];
+}
+
+- (void)getVideoCountWithCompletionHandler:(FTResponseHandler)handler {
+    NSURL *videoCountUrl = [[self baseUrl] URLByAppendingPathComponent:VideosCountResourceName];
+    NSURLRequest *request = [NSURLRequest requestWithURL:videoCountUrl];
+    [FTResourceDownloader getRequest:request withHandler:handler];
+}
+
+- (void)getFeaturedVideoWithCompletionHandler:(FTResponseHandler)handler {
+    NSURL *featuredVideoUrl = [[self baseUrl] URLByAppendingPathComponent:VideoFeaturedResourceName];
+    NSURLRequest *request = [NSURLRequest requestWithURL:featuredVideoUrl];
+    [FTResourceDownloader getRequest:request withHandler:handler];
+}
+
+- (void)getVideosForPresenter:(id)presenterId atEvent:(id)eventId withCompletionHandler:(FTResponseHandler)handler {
+    NSString *presenter = [presenterId isKindOfClass:[NSString class]] ? (NSString *)presenterId : [presenterId stringValue];
+    NSString *event = [eventId isKindOfClass:[NSString class]] ? (NSString *)eventId : [eventId stringValue];
+    NSURL *presenterUrl = [[[self baseUrl] URLByAppendingPathComponent:PresentersEndpointName] URLByAppendingPathComponent:presenter];
+    NSURL *eventUrl = [[presenterUrl URLByAppendingPathComponent:EventsEndpointName] URLByAppendingPathComponent:event];
+    NSURL *videosUrl = [eventUrl URLByAppendingPathComponent:VideosEndpointName];
+    NSURLRequest *request = [NSURLRequest requestWithURL:videosUrl];
+    [FTResourceDownloader getRequest:request withHandler:handler];
+}
 
 @end
